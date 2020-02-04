2135f0ccdd143a5dce4ad63b118fd8e10d9add1c
johannesjmeyer <jj.meyer@outlook.com>
2020-02-04 14:23:38 +0100

    Remove superfluous test

--- tests/test_simulator_device.py
+++ tests/test_simulator_device.py
@@ -55,19 +55,6 @@ def simulator_device_3_wires(shots, analytic):
     yield SimulatorDevice(3, shots=shots, analytic=analytic)
 
 @pytest.mark.parametrize("shots,analytic", [(100, True)])
-class TestInternalLogic:
-    """Test internal logic of the SimulatorDevice class."""
-
-    def test_probability_error(self, simulator_device_1_wire):
-        """Test that an error is raised in probability if the
-        internal state is None."""
-
-        simulator_device_1_wire.state = None
-
-        with pytest.raises(qml.DeviceError, match="Probability can not be computed because the internal state is None."):
-            simulator_device_1_wire.probability()
-
-@pytest.mark.parametrize("shots,analytic", [(100, True)])
 class TestApply:
     """Tests that gates are correctly applied"""
 
