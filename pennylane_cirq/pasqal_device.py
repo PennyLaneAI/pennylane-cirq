ac41fb24ebd0ddefb14ed64ff0e950baab562511
Nathan Killoran <co9olguy@users.noreply.github.com>
2020-07-08 13:11:50 -0400

    [WIP] Add pasqal device (#31)
    * adding basic pasqal device
    
    * using base simulator and removing qubit worries
    
    * adding tests
    
    * changes
    
    * merge master and fix travis issue
    
    * linting
    
    * change version
    
    * Update pennylane_cirq/pasqal_device.py
    
    Co-authored-by: Josh Izaac <josh146@gmail.com>
    
    * Update pennylane_cirq/pasqal_device.py
    
    Co-authored-by: Josh Izaac <josh146@gmail.com>
    
    * Update pennylane_cirq/pasqal_device.py
    
    Co-authored-by: Josh Izaac <josh146@gmail.com>
    
    Co-authored-by: Josh Izaac <josh146@gmail.com>

---
+++ pennylane_cirq/pasqal_device.py
@@ -0,0 +1,51 @@
+# Copyright 2018 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# pylint: disable=too-many-arguments
+"""
+This module provides the ``PasqalDevice`` from Cirq.
+"""
+from cirq import pasqal
+
+from .simulator_device import SimulatorDevice
+
+
+class PasqalDevice(SimulatorDevice):
+    r"""Cirq Pasqal device for PennyLane.
+
+    Args:
+        wires (int): the number of wires to initialize the device with
+        shots (int): Number of circuit evaluations/random samples used
+            to estimate expectation values of observables. Shots need
+            to be >= 1. In analytic mode, shots indicates the number of entries
+            that are returned by ``device.sample``.
+        analytic (bool): indicates whether expectation values and variances should
+            be calculated analytically
+        qubits (List[cirq.ThreeDGridQubit]): A list of Cirq ThreeDGridQubits that are used
+            as wires. If not specified, the ThreeDGridQubits are put in a linear
+            arrangement along the first coordinate axis,
+            i.e., ``(0,0,0), (1,0,0), (2,0,0)``, etc.
+        control_radius (float): The maximum distance between qubits for a controlled
+                gate. Distance is measured in units of the ``ThreeDGridQubit`` indices.
+    """
+    name = "Cirq Pasqal device for PennyLane"
+    short_name = "cirq.pasqal"
+
+    def __init__(self, wires, shots=1000, analytic=True, qubits=None, control_radius=1.0):
+
+        if not qubits:
+            qubits = [pasqal.ThreeDGridQubit(wire, 0, 0) for wire in range(wires)]
+        self.control_radius = float(control_radius)
+        if self.control_radius < 0:
+            raise ValueError("The control_radius must be a non-negative real number.")
+        super().__init__(wires, shots, analytic, qubits)
---
+++ tests/test_pasqal_device.py
@@ -0,0 +1,80 @@
+# Copyright 2018 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Unit tests for the PasqalDevice
+"""
+import pytest
+
+import pennylane as qml
+from pennylane_cirq import PasqalDevice, SimulatorDevice
+from cirq.pasqal import ThreeDGridQubit
+
+
+class TestDeviceIntegration:
+    """Tests that the SimulatorDevice integrates well with PennyLane"""
+
+    def test_device_loading(self):
+        """Tests that the cirq.pasqal device is properly loaded"""
+
+        dev = qml.device("cirq.pasqal", wires=2)
+
+        assert dev.num_wires == 2
+        assert len(dev.qubits) == 2
+        assert dev.shots == 1000
+        assert dev.short_name == "cirq.pasqal"
+        assert dev.analytic == True
+        assert dev.control_radius == 1.0
+        assert dev.qubits == [ThreeDGridQubit(0, 0, 0), ThreeDGridQubit(1, 0, 0)]
+        assert isinstance(dev, SimulatorDevice)
+
+
+class TestDevice:
+    """Unit tests for the PasqalDevice"""
+
+    def test_device_creation(self):
+        """Tests that the cirq.pasqal device is properly created"""
+
+        dev = PasqalDevice(wires=2, shots=123, control_radius=5.0)
+
+        assert dev.num_wires == 2
+        assert len(dev.qubits) == 2
+        assert dev.shots == 123
+        assert dev.short_name == "cirq.pasqal"
+        assert dev.analytic == True
+        assert dev.control_radius == 5.0
+        assert dev.qubits == [ThreeDGridQubit(0, 0, 0), ThreeDGridQubit(1, 0, 0)]
+        assert isinstance(dev, SimulatorDevice)
+
+    @pytest.mark.parametrize(
+        "coord_idxs",
+        [
+            [(0, 0, 0), (1, 0, 0), (2, 0, 0), (3, 0, 0)],
+            [(0, 0, 0), (0, 0, 1), (0, 1, 0), (0, 1, 1)],
+            [(1, 2, 3), (3, 2, 1), (0, 0, 0), (-1, -1, -10)],
+        ],
+    )
+    def test_device_creation(self, coord_idxs):
+        """Tests that ThreeDGridQubits can be passed as an argument"""
+
+        qubits = [ThreeDGridQubit(*idxs) for idxs in coord_idxs]
+        dev = PasqalDevice(wires=4, qubits=qubits)
+
+        assert dev.qubits == qubits
+
+    def test_control_radius_negative_exception(self):
+        """Tests that an exception is raised when the supplied control_radius parameter
+        is a negative real number"""
+
+        with pytest.raises(ValueError, match="must be a non-negative real number"):
+            dev = PasqalDevice(wires=2, shots=123, control_radius=-5.0)
