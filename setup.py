46f3b432a0a9e2a714a1314fecef861293efe215
Theodor Isacsson <theodor.isacsson@gmail.com>
2020-09-16 13:52:38 -0400

    Remove qflex from entry points

--- setup.py
+++ setup.py
@@ -37,8 +37,7 @@ info = {
     "entry_points": {"pennylane.plugins": ["cirq.simulator = pennylane_cirq:SimulatorDevice",
                                            "cirq.mixedsimulator = pennylane_cirq:MixedStateSimulatorDevice",
                                            "cirq.qsim = pennylane_cirq.qsim_device:QSimDevice",
-                                           "cirq.qsimh = pennylane_cirq.qsim_device:QSimhDevice",
-                                           "cirq.qflex = pennylane_cirq.qflex_device:QFlexDevice"],},
+                                           "cirq.qsimh = pennylane_cirq.qsim_device:QSimhDevice"],},
     # Place a one line description here. This will be shown by pip
     "description": "PennyLane plugin for Cirq",
     "long_description": open("README.rst").read(),
