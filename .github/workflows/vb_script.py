a8b869a444fa6dc0609c56facda865cc5345586c
Jay Soni <jbsoni@uwaterloo.ca>
2021-11-02 15:17:04 -0400

    Update .github/workflows/vb_script.py
    Co-authored-by: antalszava <antalszava@gmail.com>

--- .github/workflows/vb_script.py
+++ .github/workflows/vb_script.py
@@ -1,4 +1,3 @@
-"""This module implements the logic required to automatically bump the version number in the version file and update the changelog accordingly."""
 # Copyright 2018-2021 Xanadu Quantum Technologies Inc.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""This module implements the logic required to automatically bump the version number in the version file and update the changelog accordingly."""
 import argparse
 import pennylane as qml
 pl_version = '"' + qml.version() + '"'
